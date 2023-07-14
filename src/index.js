30631ca156d5a1e3ee20027c1f173384c52d08f4
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 15:33:12 +0300

    format code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import cors from 'cors';
-import dataRoutes from './routes/data.routes';
+import express from 'express'
+import cors from 'cors'
+import dataRoutes from './routes/data.routes.js'
 
-const app = express();
+const app = express()
 
-app.use(express.json({ limit: '30mb', strict: true }));
-app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb', strict: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 
-app.use(cors());
+app.use(cors())
 
-app.use('/data', dataRoutes);
+app.use('/data', dataRoutes)
 
-const PORT = '5555';
+const PORT = '5555'
 
-app.listen(PORT, () => console.log(`server running at port: ${PORT}`));
+app.listen(PORT, () => console.log(`server running at port: ${PORT}`))
--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -1,11 +1,8 @@
 import express from 'express'
-import {
-  getData,
-} from '../controllers/data.controllers'
+import { getData } from '../controllers/data.controllers.js'
 
 const router = express.Router()
 
 router.get('/all', getData)
-
 
 export default router
