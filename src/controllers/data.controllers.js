a17a166649dc62e3dfd6bd749b311c6f5e3920c3
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 15:33:33 +0300

    function to send data to client

--- src/controllers/data.controllers.js
+++ src/controllers/data.controllers.js
@@ -1,5 +1,9 @@
+import * as data from '../../enterprise-attack.json' assert {type: 'json'}
 export const getData = async (req, res) => {
-  try {
-  } catch (err) {
-  }
+
+    try {
+      return res.status(200).json({data})
+    } catch (err) {
+      return res.status(500).json({message: err.message})
+    }
 }
