7e711841f58b2062a82f8b5e5ba9f33da7995445
islamCodehood <islam.sayed8@gmail.com>
2023-07-14 16:17:30 +0300

    format code

--- src/controllers/data.controllers.js
+++ src/controllers/data.controllers.js
@@ -1,9 +1,8 @@
-import * as data from '../../enterprise-attack.json' assert {type: 'json'}
+import * as data from '../../enterprise-attack.json' assert { type: 'json' }
 export const getData = async (req, res) => {
-
     try {
-      return res.status(200).json({data})
+        return res.status(200).json({ data })
     } catch (err) {
-      return res.status(500).json({message: err.message})
+        return res.status(500).json({ message: err.message })
     }
 }
